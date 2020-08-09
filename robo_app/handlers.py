23f0b29a313a5d09ada590ca7f786e0b5fe12cb7
Aleksey Shilo <chief_exb@mail.ru>
2020-08-09 13:41:30 +0300

    Fix

--- robo_app/handlers.py
+++ robo_app/handlers.py
@@ -2,7 +2,7 @@ from textwrap import dedent
 import json
 from aiohttp import web , ClientSession
 from aiohttp_security.api import AUTZ_KEY
-from aiohttp_security import remember, forget
+#from aiohttp_security import remember, forget
 from security import *
 
 #from aiohttp_security import (
